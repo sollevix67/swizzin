f72aabe3a07d51a17661525256c2a705aacbc450
sollevix67 <7199110+sollevix67@users.noreply.github.com>
2023-11-17 21:15:58 +0100

    Update webmin.sh
    Changed Webmin repo and GPG key to the new ones

--- scripts/install/webmin.sh
+++ scripts/install/webmin.sh
@@ -5,8 +5,8 @@
 
 _install_webmin() {
     echo_progress_start "Installing Webmin repo"
-    echo "deb [signed-by=/usr/share/keyrings/webmin-archive-keyring.gpg] https://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list
-    curl -s https://download.webmin.com/jcameron-key.asc | gpg --dearmor > /usr/share/keyrings/webmin-archive-keyring.gpg 2>> "${log}"
+    echo "deb [signed-by=/usr/share/keyrings/webmin-archive-keyring.gpg] https://download.webmin.com/download/newkey/repository stable contrib" > /etc/apt/sources.list.d/webmin.list
+    curl -s https://download.webmin.com/developers-key.asc | gpg --dearmor > /usr/share/keyrings/debian-webmin-developers.gpg 2>> "${log}"
     echo_progress_done "Repo added"
     apt_update
     apt_install webmin
